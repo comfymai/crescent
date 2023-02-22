963ff0078ade39c0d2c681788d6376be7f182ad4
Maya Lira <mkzk.mail@gmail.com>
2023-02-22 15:57:26 -0300

    feat: create discord connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { GatewayIntentBits } from "discord.js";
+import { NecordModule } from "necord";
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [
+        ConfigModule.forRoot(),
+        NecordModule.forRoot({
+            token: process.env.BOT_TOKEN ?? "",
+            intents: [GatewayIntentBits.Guilds],
+        }),
+    ],
 })
 export class AppModule {}
