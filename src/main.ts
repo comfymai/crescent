9d78a7e6695a2bde24d77c5162cf1f31cd6451a2
Maya Lira <mkzk.mail@gmail.com>
2023-02-23 15:49:21 -0300

    refactor/deps: remove unused dependencies (express)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import "dotenv/config"
+import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 (async () => {
-    await (await NestFactory.create(AppModule)).listen(3123);
+    await NestFactory.createApplicationContext(AppModule);
 })();
