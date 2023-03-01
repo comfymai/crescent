0306aae4b3b88eee0b404d022978acd6129cbc42
Maya Lira <mkzk.mail@gmail.com>
2023-03-01 00:50:34 -0300

    feat@youtube: implemented join on play call

--- src/books/youtube/spells/playback.ts
+++ src/books/youtube/spells/playback.ts
@@ -1,16 +1,20 @@
+import { Nullable } from "@app/helpers/types";
+import { Logger } from "@nestjs/common";
+import { ChannelType } from "discord.js";
 import { Context, Options, SlashCommandContext, Subcommand } from "necord";
 
 import { YoutubeGroup } from ".";
-import { SessionManager } from "../session.manager";
+import { Session, SessionManager } from "../session.manager";
 import { PlayIngredients } from "./ingredients/playback";
 
 @YoutubeGroup()
 export class PlaybackSpells {
+    private readonly logger = new Logger(PlaybackSpells.name)
     constructor(private sessions: SessionManager) {}
 
     @Subcommand({
         name: "play",
-        description: "Plays the audio from a YouTube video."
+        description: "Plays the audio from a YouTube video.",
     })
     public async playTrack(
         @Context() [interaction]: SlashCommandContext,
@@ -23,13 +27,38 @@ export class PlaybackSpells {
                 ephemeral: true,
             });
 
-        const session = this.sessions.getById(guild.id);
-        if (session == null)
-            // TODO: automatically call join here.
-            return interaction.reply({
-                content: "There's no voice connections in this guild.",
-                ephemeral: true,
-            });
+        const session = <Nullable<Session>> await (async () => {
+            const existingSession = this.sessions.getById(guild.id)
+            if (existingSession) return existingSession
+
+            const member = await guild.members.fetch(interaction.user);
+            if (!member) {
+                this.logger.warn("Failed to find member.");
+                interaction.reply(
+                    "Failed to find you, try again later."
+                );
+                return null
+            }
+
+            if (member.voice.channel == null) {
+                interaction.reply(
+                    "You must be in a voice channel to use this command."
+                );
+                return null
+            }
+
+            if (member.voice.channel.type != ChannelType.GuildVoice) {
+                interaction.reply(
+                    `You must be in a normal voice channel!`
+                );
+                return null
+            }
+
+            const session = this.sessions.create(member.voice.channel);
+            return session
+        })()
+
+        if (session == null) return;
 
         const connection = session.connection;
         if (connection == null)
@@ -39,8 +68,9 @@ export class PlaybackSpells {
                 ephemeral: true,
             });
 
-        const trackStatus = session.addTrack(url)
-        if (trackStatus == 'playing') return interaction.reply(`Now playing track from: ${url}.`);
-        else return interaction.reply(`Track queued: ${url}.`)
+        const trackStatus = session.addTrack(url);
+        if (trackStatus == "playing")
+            return interaction.reply(`Now playing track from: ${url}.`);
+        else return interaction.reply(`Track queued: ${url}.`);
     }
 }
